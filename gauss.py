0b58695aa0a8ea58ec934ce16db77cc95c7d8ed7
gauss-allianz-user <service@gauss-allianz.de>
2017-01-18 12:47:21 +0100

    Functions for module upload + module list parsing

--- gauss.py
+++ gauss.py
@@ -1,1 +1,47 @@
 # gauss.py - python module to connect to module uplod api
+import json, requests
+
+def module_upload(apikey, modules, api_path, delete_old_modules):
+    data = {
+        "apikey": apikey,
+        "delete_old_modules": delete_old_modules,
+        "modules": modules
+    }
+
+    return requests.post(api_path, data = { "json": json.dumps(data) })
+
+# parse_module_list expects a module list generated
+# by the command: "module available -t"
+# Expected format: "module_name/version"
+def parse_module_list(module_list):
+    modules = []
+
+    for line in module_list.split("\n"):
+        # convention: module name starts with a-zA-Z
+        # skip all others
+        if line and line[0].isalpha():
+            name = line.split("/", 1)[0]
+            # check if version information exists and
+            # only add module in that case
+            if len(line.split("/", 1)) == 2:
+                version = line.split("/", 1)[1]
+                modules.append(
+                    {
+                        "name": name,
+                        "version": version
+                    }
+                )
+
+    return modules
+
+# parse_module_file expects a file with a module list generated
+# by the command: "module available -t"
+# Expected format: "module_name/version"
+def parse_module_list_file(module_file):
+    modules = []
+
+    # open module file
+    with open(module_file) as f:
+        content = f.read()
+
+    return parse_module_list(content)
